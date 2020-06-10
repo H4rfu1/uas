e0ae8b025605700ad14efe5a63a43345902620a7
H4rfu1 <mohfahrulhafidh@gmail.com>
2020-06-10 19:25:00 +0700

    model 1

--- app/config/config.php
+++ app/config/config.php
@@ -5,5 +5,5 @@ define('BASEURL', 'http://localhost/uas/uas');
 // DB
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASS', 'root');
-define('DB_NAME', 'phpmvc');
+define('DB_PASS', '');
+define('DB_NAME', 'cardoor');
