57c5fde1ed86690848326c877b015d8a53506910
H4rfu1 <mohfahrulhafidh@gmail.com>
2020-09-30 20:38:11 +0700

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -1,9 +1,9 @@
 <?php
 
-define('BASEURL', 'http://localhost/uas/uas');
+define('BASEURL', 'http://b.pbw.ilkom.unej.ac.id/182410101109/uas/');
 
 // DB
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'cardoor');
+define('DB_USER', '182410101109');
+define('DB_PASS', 'secret');
+define('DB_NAME', 'uas182410101109');
